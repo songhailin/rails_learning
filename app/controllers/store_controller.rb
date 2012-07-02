b5a79de28a4d23df1c0845097d7a201eade27da1
songhailin <songhailin@gmail.com>
2012-07-02 16:09:58 +0800

    add session store

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,5 +3,20 @@ class StoreController < ApplicationController
     @products = Product.find_products_by_sale
   end
 
+  def add_to_cart
+    product = Product.find(params[:id])
+    @cart = find_cart
+    @cart.add_product(product)
+  end
+
+
+  private
+  
+  def find_cart
+    unless session[:cart]
+      session[:cart] = Cart.new
+    end
+    session[:cart]
+  end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120529023401) do
+ActiveRecord::Schema.define(:version => 20120702062753) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -39,5 +39,15 @@ ActiveRecord::Schema.define(:version => 20120529023401) do
     t.datetime "updated_at",                                                 :null => false
     t.decimal  "price",       :precision => 8, :scale => 2, :default => 0.0
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
 end
